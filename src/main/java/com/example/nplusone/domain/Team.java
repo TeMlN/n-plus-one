83e075d936750e3da7288b401d687ad242b1021a
taem1n1 <youman7953@naver.com>
2022-04-05 19:30:05 +0900

    Team 엔티티 생성

---
+++ src/main/java/com/example/nplusone/domain/Team.java
@@ -0,0 +1,29 @@
+package com.example.nplusone.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter @Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class Team {
+
+    @Column(name = "team_id")
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "team_name")
+    private String teamName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member")
+    private Member member;
+}
