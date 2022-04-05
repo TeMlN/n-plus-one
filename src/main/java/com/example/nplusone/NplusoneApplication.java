fe9bc5a70f740e72c8ff96fadb7ad9e74dac4ea8
taem1n1 <youman7953@naver.com>
2022-04-05 19:22:44 +0900

    initial commit

---
+++ src/main/java/com/example/nplusone/NplusoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.nplusone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NplusoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NplusoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nplusone/NplusoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nplusone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NplusoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
