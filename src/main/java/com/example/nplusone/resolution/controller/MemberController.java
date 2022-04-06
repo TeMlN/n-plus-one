a4205fc9b08339c957b2e30d44b068ccd0c7fd1e
taem1n1 <youman7953@naver.com>
2022-04-07 07:56:11 +0900

    DataJPA 로 해결하기

--- src/main/java/com/example/nplusone/resolution/controller/MemberController.java
+++ src/main/java/com/example/nplusone/resolution/controller/MemberController.java
@@ -1,9 +1,8 @@
-package com.example.nplusone.domain.member.controller;
+package com.example.nplusone.resolution.controller;
 
 import com.example.nplusone.domain.member.Member;
 import com.example.nplusone.domain.member.repository.MemberRepository;
-import com.example.nplusone.domain.team.Team;
-import com.example.nplusone.domain.team.repository.TeamRepository;
+import com.example.nplusone.resolution.fetch_join.data_jpa.DataJpaMemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +20,11 @@ import java.util.List;
 public class MemberController {
 
     private final MemberRepository memberRepository;
+    private final DataJpaMemberRepository dataJpaMemberRepository;
 
+    /**
+     * N+1 예제
+     */
     @GetMapping("/get-members-team")
     public void getMembersTeam() {
         List<Member> members = memberRepository.findAll();
@@ -33,6 +36,20 @@ public class MemberController {
             // member의 team에 접근을 하면 프록시 객체가 아닌 db에서 다시 불러와진다
             // 하지만 위 코드를 보면 member 1명마다 그 member의 team을 조회하기 때문에 member한명당 한번의 쿼리가 발생한다 이를 N+1이라 한다
             // 이를 해결하기 위해선 member들의 team을 전체조회 하는게 해결방법이다
+        }
+    }
+
+
+    /**
+     * Data JPA로 해결하기 (fetch join)
+     */
+    @GetMapping("/get-members-team/data-jpa")
+    public void getMembersTeamDataJpa() {
+        List<Member> members = dataJpaMemberRepository.findAll();
+
+        for (Member member : members) {
+            System.out.println("member.getMemberName() = " + member.getMemberName()); // 단순 member를 조회하기 때문에 쿼리 1개발생
+            System.out.println("member.getTeam().getTeamName() = " + member.getTeam().getTeamName());
         }
     }
 
