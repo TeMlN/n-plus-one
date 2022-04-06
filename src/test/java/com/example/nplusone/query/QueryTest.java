4c58b2893f3d35db88f14f998755c0b20b306a10
taem1n1 <youman7953@naver.com>
2022-04-07 07:56:25 +0900

    import 삭제

--- src/test/java/com/example/nplusone/query/QueryTest.java
+++ src/test/java/com/example/nplusone/query/QueryTest.java
@@ -2,18 +2,12 @@ package com.example.nplusone.query;
 
 import com.example.nplusone.domain.member.Member;
 import com.example.nplusone.domain.member.repository.MemberRepository;
-import com.example.nplusone.domain.team.Team;
-import com.example.nplusone.domain.team.repository.TeamRepository;
-import org.apache.logging.slf4j.SLF4JLogger;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.List;
-import java.util.logging.Logger;
 
 @SpringBootTest
 @Transactional
