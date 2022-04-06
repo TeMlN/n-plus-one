d92d31bb348ecd1bfdc7b58aa26ef4f28997d412
taem1n1 <youman7953@naver.com>
2022-04-06 16:13:44 +0900

    memberRepository

--- src/main/java/com/example/nplusone/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/nplusone/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.example.nplusone.domain.member.repository;
 
 import com.example.nplusone.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
