e4e9db79f00b3b98e4e769baf262e7d71686a44e
taem1n1 <youman7953@naver.com>
2022-04-07 07:56:38 +0900

    DataJPA로 해결하기

---
+++ src/main/java/com/example/nplusone/resolution/fetch_join/data_jpa/DataJpaMemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.nplusone.resolution.fetch_join.data_jpa;
+
+import com.example.nplusone.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface DataJpaMemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select m from Member m join m.team")
+    List<Member> findAllWithDataJpa();
+}
