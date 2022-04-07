9a8d8f07f5e1a4769dbaffb9d542b35a9a91d523
taem1n1 <youman7953@naver.com>
2022-04-07 17:09:27 +0900

    Querydsl config

--- src/main/java/com/example/nplusone/config/querydsl/QuerydslConfiguration.java
+++ src/main/java/com/example/nplusone/config/querydsl/QuerydslConfiguration.java
@@ -1,2 +1,20 @@
-package com.example.nplusone.config.querydsl;public class QuerydslConfiguration {
+package com.example.nplusone.config.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfiguration {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(em);
+    }
 }
